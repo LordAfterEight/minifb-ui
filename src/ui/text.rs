3b2135e20d54dd942ca973cf1fb221236a527fad
LordAfterEight <elias@stettmayer.de>
2026-03-28 22:03:44 +0100

    Implement get_width method for Text

--- src/ui/text.rs
+++ src/ui/text.rs
@@ -10,6 +10,14 @@ impl Text {
             text: text.to_string()
         }
     }
+
+    pub fn get_width(&self, size: f32) -> usize {
+        let mut width = 0;
+        for c in self.text.chars() {
+            width += self.font.font.metrics(c, size).width;
+        }
+        return width;
+    }
 }
 
 impl Default for Text {
