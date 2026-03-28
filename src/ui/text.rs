3ae578324d221ee6739978e36c76cb42149d404c
LordAfterEight <elias@stettmayer.de>
2026-03-28 22:07:52 +0100

    Fix f32 to usize assignment

--- src/ui/text.rs
+++ src/ui/text.rs
@@ -14,7 +14,7 @@ impl Text {
     pub fn get_width(&self, size: f32) -> usize {
         let mut width = 0;
         for c in self.text.chars() {
-            width += self.font.font.metrics(c, size).advance_width;
+            width += self.font.font.metrics(c, size).advance_width as usize;
         }
         return width;
     }
